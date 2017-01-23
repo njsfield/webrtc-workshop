f0447f942f495526baebbed6435b0cb2d9bc2c65
msachi <marchee88@gmail.com>
2017-01-23 12:23:44 +0000

    Serve index.html

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const options = {
   cert: fs.readFileSync('keys/cert.pem')
 };
 
+const html = fs.readFileSync('index.html');
+
 https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end('Great success');
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write(html);
+  res.end();
 }).listen(8000, function(){
   console.log('Server running at https://localhost:8000/');
 });
